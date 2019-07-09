9853e60a1f940fbfebcad190dc2c102991221cdf
Rachel Haub <rachelhaub@gmail.com>
2019-07-09 18:17:30 -0400

    Basic inventory system
    Not Debugged yet: No character movement

---
+++ Fairy Game Prototyping/Assets/Slot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject item;
+    public string type;
+    public int id;
+    public string description;
+    public bool empty;
+    public Sprite icon;
+
+    public void UpdateSlot()
+    {
+        this.GetComponent<Image>().sprite = icon;
+    }
+}
