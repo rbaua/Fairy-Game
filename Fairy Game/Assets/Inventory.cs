d5300a8b5b9688f4c3cbbe5bcd9494f51424244e
Rachel Haub <rachelhaub@gmail.com>
2019-07-26 12:52:46 -0400

    Migrated to 3D

---
+++ Fairy Game/Assets/Inventory.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    private bool inventoryEnabled;
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slots;
+
+    public GameObject slotHolder;
+
+    void Start()
+    {
+        allSlots = 42;
+        slots = new GameObject[allSlots];
+
+        for(int i=0; i<allSlots; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+            if(slots[i].GetComponent<Slot>().item == null)
+            {
+                slots[i].GetComponent<Slot>().empty = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if(inventoryEnabled)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Item")
+        {
+            GameObject itemTouched = other.gameObject;
+            Item item = itemTouched.GetComponent<Item>();
+            AddItem(itemTouched, item.id, item.type, item.description, item.icon);
+        }
+    }
+
+    void AddItem(GameObject itemObject, int itemID, string itemType, string itemDescription, Sprite itemIcon)
+    {
+        for(int i =0; i<allSlots; i++)
+        {
+            if(slots[i].GetComponent<Slot>().empty)
+            {
+                itemObject.GetComponent<Item>().pickedUp = true;
+
+                slots[i].GetComponent<Slot>().icon = itemIcon;
+                slots[i].GetComponent<Slot>().type = itemType;
+                slots[i].GetComponent<Slot>().id = itemID;
+                slots[i].GetComponent<Slot>().description = itemDescription;
+                slots[i].GetComponent<Slot>().item = itemObject;
+
+                itemObject.transform.parent = slots[i].transform;
+                itemObject.SetActive(false);
+
+                slots[i].GetComponent<Slot>().UpdateSlot();
+                slots[i].GetComponent<Slot>().empty = false;
+                return;
+            }
+            
+        }
+    }
+}
---
+++ Fairy Game/Assets/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string type;
+    public int id;
+    public string description;
+    public Sprite icon;
+    public bool pickedUp;
+}
